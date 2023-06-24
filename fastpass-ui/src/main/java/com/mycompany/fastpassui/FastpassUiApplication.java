a5e8474c5046cfe3edf9a706a5025df1ada11631
saibhagawan139 <118386739+saibhagawan139@users.noreply.github.com>
2023-06-24 07:48:19 -0700

    Add files via upload

---
+++ fastpass-ui/src/main/java/com/mycompany/fastpassui/FastpassUiApplication.java
@@ -0,0 +1,22 @@
+package com.mycompany.fastpassui;
+
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FastpassUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastpassUiApplication.class, args);
+	}
+
+	@Bean
+	public Supplier<FastPassToll> generatetollCharge() {
+		return () -> new FastPassToll("800", "1001", 1.05f);
+
+	}
+
+}
